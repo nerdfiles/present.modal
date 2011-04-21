bde7baa71eca5e80d404f9fcee70901548f5c5d8
Aaron Alexander <nerdfiles@gmail.com>
2011-04-20 23:30:34 -0500

    update

--- _assets/_js/main.js
+++ _assets/_js/main.js
@@ -27,17 +27,53 @@ jQuery(function($) {
             
             $( 'div#' + id ).before('<div class="modal-overlay"></div>');
             
+            $( 'div#' + id ).addClass('active-modal-frame');
+            $( 'div#' + id ).prev().addClass('active-modal-overlay');
+            
+            if ( !$( 'div#' + id ).find('.controls .modal-cancel').length ) {
+                $( 'div#' + id ).find('.controls ul').append('<li><input type="button" class="button modal-cancel" name="modal-cancel" value="Cancel" /></li>');
+            }
+            
             $( 'div#' + id ).prev().fadeIn();
             
-            $( 'div#' + id ).animate({
-                opacity: 'show'
+            $( 'div#' + id ).hide();
+            
+            $( 'div#' + id ).css({
+                marginLeft: '0'
             });
             
+            $( 'div#' + id ).animate({
+                'opacity': 'show'
+            }, 500);
+            
             $('html').addClass('modal-visible');
+            
+            var oldform = $('.active-modal-frame').find('form').clone();
+            $( 'div#' + id ).data('oldform', oldform);
             
             e.preventDefault();
         }); 
     
+    });
+    
+    $('.controls a.modal-cancel').live('click', function(e) {
+        
+    });
+    
+    $('.controls input.modal-cancel').live('click', function(e) {
+        var $self = $(this),
+            placeElem = $('.active-modal-frame').find('form').prev();
+            
+        var id = $('.active-modal-frame').attr('id');
+        
+        $('.active-modal-frame').find('form').remove();
+        var oldform = $( 'div#' + id ).data('oldform');
+        $(placeElem).after(oldform);
+        
+        $('.active-modal-frame').fadeOut(300).removeClass('active-modal-frame');
+        $('.active-modal-overlay').fadeOut(300, function() {
+            $(this).remove();
+        });
     });
     
     // modal overlay

86825b6dffd75e6da63e0e7c058b70c2156e43be
Aaron Alexander <nerdfiles@gmail.com>
2011-04-20 22:00:57 -0500

    update

--- _assets/_js/main.js
+++ _assets/_js/main.js
@@ -6,8 +6,50 @@
  
 jQuery(function($) {
 
-    $('a').bind('click', function(e) {
-        e.preventDefault();
+    $('legend').hide();
+
+    /**
+     * modal overlay
+     *
+     * If disabled the assistive technology should follow the internal anchor to its
+     * destination anyway. So testing JavaScript enabled and disabled amount to the same
+     * thing. (TN!!)
+     */
+    
+    $('a.modal-open').each(function() {
+    
+        var $self = $(this);
+    
+    
+        $self.bind('click', function(e) {
+            
+            var id = $(this).attr('href').match(/#(.*)/)[1];
+            
+            $( 'div#' + id ).before('<div class="modal-overlay"></div>');
+            
+            $( 'div#' + id ).prev().fadeIn();
+            
+            $( 'div#' + id ).animate({
+                opacity: 'show'
+            });
+            
+            $('html').addClass('modal-visible');
+            
+            e.preventDefault();
+        }); 
+    
+    });
+    
+    // modal overlay
+    
+    $('a.modal-frame').each(function() {
+    
+        var $self = $(this);
+    
+        $self.bind('click', function(e) {
+            e.preventDefault();
+        });
+    
     });
 
 });
